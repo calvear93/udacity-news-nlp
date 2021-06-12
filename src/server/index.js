906063acff5d8a421ffe2b2757b8fc0c43c23429
Cristopher Alvear <c_alvear@live.cl>
2021-06-12 18:15:28 -0400

    chore: eslint

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,29 @@
 const dotenv = require('dotenv');
-dotenv.config();
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 const ailientendpoint = 'https://api.aylien.com/news';
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res)
+{
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+    res.sendFile(path.resolve('src/client/views/index.html'));
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8080, function()
+{
+    console.log('Example app listening on port 8080!');
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get('/test', function(req, res)
+{
+    res.send(mockAPIResponse);
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,7 +13,11 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+              }
         ]
     },
     plugins: [
@@ -19,5 +25,12 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         })
-    ]
+    ],
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+          // `...`,
+          new CssMinimizerPlugin(),
+        ],
+      },
 }
