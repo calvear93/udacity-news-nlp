bbcd61cce6850dafd0e30bf5f6f00cf9e6db1e40
Cristopher Alvear <c_alvear@live.cl>
2021-06-19 18:40:13 -0400

    feat: page working

--- src/server/routes/form.routes.js
+++ src/server/routes/form.routes.js
@@ -2,6 +2,8 @@ import { analyzeArticle } from '../services/nlp.service';
 
 export async function exec({ body: { url } }, response)
 {
+    console.log('url', url, 'received');
+
     const { agreement, irony, sentence_list } = await analyzeArticle(url);
 
     response.send({
@@ -11,8 +13,7 @@ export async function exec({ body: { url } }, response)
             .map(i => ({
                 text: i.text,
                 agreement: i.agreement,
-                keywords: i.sentimented_concept_list
-                    .map(s => s.form)
+                keywords: [ ...new Set(i.sentimented_concept_list.map(s => s.form)) ] // avoid duplicates
             }))
             .filter(i => i.keywords?.length > 0)
     });
